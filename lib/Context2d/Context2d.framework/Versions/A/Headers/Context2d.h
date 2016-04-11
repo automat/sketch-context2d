b242c87d8bce2cb154a849724a347f5c91b2ebe3
Henryk Wollik <olafyoorsen@hotmail.com>
2016-04-12 00:14:46 +0200

    lib Context2d build added

---
+++ lib/Context2d/Context2d.framework/Versions/A/Headers/Context2d.h
@@ -0,0 +1,20 @@
+//
+//  Context2d.h
+//  Context2d
+//
+//  Created by Henryk Wollik on 10/04/16.
+//  Copyright © 2016 automat. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Context2d.
+FOUNDATION_EXPORT double Context2dVersionNumber;
+
+//! Project version string for Context2d.
+FOUNDATION_EXPORT const unsigned char Context2dVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Context2d/PublicHeader.h>
+
+#import <Context2d/ATContext2dJS.h>
+
