a190c6b8a1b55cf1673bc234f07edc33d29658d3
Henryk Wollik <olafyoorsen@hotmail.com>
2016-04-15 18:32:16 +0200

    02-fillText added test for default fonts

--- test/CanvasRenderingContext2d-API/02-fillText.js
+++ test/CanvasRenderingContext2d-API/02-fillText.js
@@ -3,7 +3,12 @@
 function main(canvas){
     var ctx = canvas.getContext('2d');
     ctx.font = "48px sans-serif";
+
     ctx.fillText("Hello world", 50, 100);
+    ctx.font = "48px serif";
+    ctx.fillText("Hello world", 50, 150);
+    ctx.font = "48px monospace";
+    ctx.fillText("Hello world", 50, 200);
 }
 
 module.exports = main;
