6b8e7e8855424ea4d45a2f6bdd8ee048936b3091
Henryk Wollik <olafyoorsen@hotmail.com>
2016-04-17 00:21:21 +0200

    CanvasRenderingContext2d-API image data tests added

--- test/CanvasRenderingContext2d-API/43-createImageData.js
+++ test/CanvasRenderingContext2d-API/43-createImageData.js
@@ -1,1 +1,12 @@
-//TODO: add
+//https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/createImageData//https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/globalCompositeOperation
+
+function main(canvas){
+    var ctx = canvas.getContext("2d");
+
+    ctx.rect(10, 10, 100, 100);
+    ctx.fill();
+
+    console.log(ctx.createImageData(100, 100));
+}
+
+module.exports = main;
--- test/CanvasRenderingContext2d-API/45-putImageData.js
+++ test/CanvasRenderingContext2d-API/45-putImageData.js
@@ -1,1 +1,15 @@
-//TODO: add
+//https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/putImageData
+
+function main(canvas){
+    var ctx = canvas.getContext("2d");
+
+    ctx.rect(10, 10, 100, 100);
+    ctx.fill();
+
+    var imageData = ctx.getImageData(0,0,100,100);
+    console.log(imageData.data);
+
+    ctx.putImageData(imageData,150,0,50,50,25,25)
+}
+
+module.exports = main;
