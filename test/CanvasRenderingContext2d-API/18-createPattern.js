66ac116f3bf5733ca2d1dc4cbd69839bc50ebc24
Henryk Wollik <olafyoorsen@hotmail.com>
2016-07-16 18:41:53 +0200

    test added 18-createPattern

--- test/CanvasRenderingContext2d-API/18-createPattern.js
+++ test/CanvasRenderingContext2d-API/18-createPattern.js
@@ -1,3 +1,23 @@
 //https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/createPattern
 
-//TODO: add
+const PATH_IMAGE = 'assets/test-image.png';
+
+function main(canvas){
+    var img = new Image();
+    function draw(){
+        var ctx = canvas.getContext('2d');
+
+        ctx.drawImage(img,0,0);
+    }
+
+    if(sketch){
+        img.src = __dirnamePlugin + '/' + PATH_IMAGE;
+        draw();
+    } else {
+        img.onload = draw;
+        img.src = './' + PATH_IMAGE;
+    }
+}
+
+module.exports = main;
+
