b2ac4f82761123f886389f4d7fd6d41886ae1691
Henryk Wollik <olafyoorsen@hotmail.com>
2016-07-16 18:42:31 +0200

    tests 00-fillRect cosmetics

--- test/CanvasRenderingContext2d-API/00-fillRect.js
+++ test/CanvasRenderingContext2d-API/00-fillRect.js
@@ -4,6 +4,15 @@ function main(canvas){
     var ctx = canvas.getContext('2d');
     ctx.fillStyle = "green";
     ctx.fillRect(10, 10, 100, 100);
+    ctx.fillStyle = 'red';
+    ctx.fillRect(10,10,50,50);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(10,10,25,25);
+    ctx.fillStyle = '#fff';
+    ctx.fillRect(10,10,12.5,12.5);
+    ctx.fillStyle = '#00ff00';
+    ctx.fillRect(10,10,30,30);
+
 }
 
 module.exports = main;
