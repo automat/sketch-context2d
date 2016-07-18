76bb972c9ac58efbb874c916b0f1e1d99476560b
Henryk Wollik <olafyoorsen@hotmail.com>
2016-07-18 09:50:04 +0200

    bin cmd exposed max-buffer option

--- bin/cmd.js
+++ bin/cmd.js
@@ -7,8 +7,9 @@ if(argv['help']){
     console.log('--help     ',   'Show all argument options');
     console.log('--verbose  ',   'Log process');
     console.log('--log-verbose ','Log js script line number and columns');
-    console.log('--recreate ',   'Don´t override target groups, create new ones')
+    console.log('--recreate ',   'Don´t override target groups, create new ones');
     console.log('--flatten  ',   'Flattens resulting path group to image');
+    console.log('--max-buffer',  'Largest amount of data (in bytes) allowed on stdout or stderr. (Default: 200*1024)');
     console.log('--watch    ',   'Watch js script');
     console.log('');
 }
@@ -22,8 +23,9 @@ if(!argv._.length){
 const file = path.resolve(argv._[0]);
 require('../index.js')([file],{
     verbose    : !!argv['verbose'],
-    verboseLog : !!argv['verboseLog'],
+    verboseLog : !!argv['log-verbose'],
     recreate   : !!argv['recreate'],
     flatten    : !!argv['flatten'],
+    maxBuffer  : argv['max-buffer'],
     watch      : !!argv['watch']
 });
