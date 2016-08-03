3b12f381f4f7487f3ddc2117ae36e81728a9995f
Henryk Wollik <olafyoorsen@hotmail.com>
2016-08-03 11:39:47 +0300

    bin cmd exposed artboard auto-selection

--- bin/cmd.js
+++ bin/cmd.js
@@ -4,13 +4,14 @@ const path = require('path');
 
 //help
 if(argv['help']){
-    console.log('--help     ',   'Show all argument options');
-    console.log('--verbose  ',   'Log process');
-    console.log('--log-verbose ','Log js script line number and columns');
-    console.log('--recreate ',   'Don´t override target groups, create new ones');
-    console.log('--flatten  ',   'Flattens resulting path group to image');
-    console.log('--max-buffer',  'Largest amount of data (in bytes) allowed on stdout or stderr. (Default: 200*1024)');
-    console.log('--watch    ',   'Watch js script');
+    console.log('--help           ','Show all argument options');
+    console.log('--verbose        ','Log process');
+    console.log('--log-verbose    ','Log js script line number and columns');
+    console.log('--select-artboard','Automatically select the first artboard if none selected.');
+    console.log('--recreate       ','Don´t override target groups, create new ones');
+    console.log('--flatten        ','Flattens resulting path group to image');
+    console.log('--max-buffer     ','Largest amount of data (in bytes) allowed on stdout or stderr. (Default: 200*1024)');
+    console.log('--watch          ','Watch js script');
     console.log('');
 }
 
@@ -22,10 +23,11 @@ if(!argv._.length){
 
 const file = path.resolve(argv._[0]);
 require('../index.js')([file],{
-    verbose    : !!argv['verbose'],
-    verboseLog : !!argv['log-verbose'],
-    recreate   : !!argv['recreate'],
-    flatten    : !!argv['flatten'],
-    maxBuffer  : argv['max-buffer'],
-    watch      : !!argv['watch']
+    verbose      : !!argv['verbose'],
+    verboseLog   : !!argv['log-verbose'],
+    autoArtboard : !!argv['select-artboard'],
+    recreate     : !!argv['recreate'],
+    flatten      : !!argv['flatten'],
+    maxBuffer    : argv['max-buffer'],
+    watch        : !!argv['watch']
 });
