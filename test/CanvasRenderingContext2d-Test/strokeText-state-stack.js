b1f4ffb93bd70b4dad35aa22b225db7b3eea941a
Henryk Wollik <olafyoorsen@hotmail.com>
2016-08-03 15:55:36 +0300

    CanvasRenderingContext2d-Test strokeText state test added

---
+++ test/CanvasRenderingContext2d-Test/strokeText-state-stack.js
@@ -0,0 +1,38 @@
+module.exports = function main(canvas){
+    var width  = canvas.width;
+    var height = canvas.height;
+    var ctx = canvas.getContext('2d');
+
+    var num = 100;
+
+    var areaWidth = width / 3;
+    var areas = [
+        [0,0,areaWidth,height],
+        [areaWidth,0,areaWidth,height],
+        [areaWidth*2,0,areaWidth,height]
+    ];
+
+    var fonts = [
+        '16px sans-serif',
+        '32px serif',
+        '12px monospace'
+    ];
+
+    function drawText(font,bounds){
+        ctx.save();
+        ctx.font = font;
+        var x = bounds[0];
+        var y = bounds[1];
+        var w = bounds[2];
+        var h = bounds[3];
+        for(var i = 0; i < num; ++i){
+            var n = i / (num - 1);
+            ctx.strokeText(n.toFixed(2),x + Math.random() * w, y + Math.random() * h);
+        }
+        ctx.restore();
+    }
+
+    for(var i = 0; i < areas.length; ++i){
+        drawText(fonts[i],areas[i]);
+    }
+};
